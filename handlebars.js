b7a3cda4dd56de8b8ccbdb76eb3ecec2b3dbaa1b
Ethan Daniel <e.daniel.small@gmail.com>
2023-05-31 11:34:15 -0700

    added part 2 of handlebars test

---
+++ handlebars.js
@@ -0,0 +1,13 @@
+function fill_template(){
+    var data = {
+        title: "why handlebars is cool",
+        list: [
+            {name: "reason one"},
+            {name: "reason two"},
+        ],
+        footer: "this is a footer"
+    };
+    var template = Handlebars.compile(document.querySelector("#template").innerHTML);
+    var filled = template(data)
+    document.querySelector("#output").innerHTML = filled
+};
