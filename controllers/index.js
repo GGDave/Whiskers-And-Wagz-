84fd03b696c20ea8f62ca543f17ac68d72b59764
David Flores <dcflores@live.com>
2023-06-01 21:34:14 -0700

    modified handle bars

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,16 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-
+const { Pets } = require('../models');
 router.use('/api', apiRoutes);
+
+
+router.get('/', async (req, res) => {
+    try {
+      const petsData = await Pets.findAll();
+      res.render("pets", { pets: petsData.map((pets) => pets.get({ plain: true })) });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
