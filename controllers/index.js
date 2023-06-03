b379110b679f67ca6beff33a15e7f553c33ff1e7
David Flores <dcflores@live.com>
2023-06-02 20:24:57 -0700

    corrected seeding issues

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,13 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
-const { Pets } = require('../models');
+const { Types } = require('../models');
 router.use('/api', apiRoutes);
 
 
 router.get('/', async (req, res) => {
     try {
-      const petsData = await Pets.findAll();
-      res.render("pets", { pets: petsData.map((pets) => pets.get({ plain: true })) });
+      const petsData = await Types.findAll();
+      res.render("types", { types: petsData.map((types) => types.get({ plain: true })) });
     } catch (err) {
       res.status(500).json(err);
     }
