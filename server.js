e9e915f6109b032c080490075be60787129261e6
BreeAnn <breeannbond@gmail.com>
2023-08-17 13:03:56 -0700

    server bug

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const session = require('express-session');
-const app = express();
-const PORT = process.env.PORT || 3001;
 const path = require('path');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const exphbs = require('express-handlebars');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 // Create an instance of the Handlebars engine
 const hbs = exphbs.create();
